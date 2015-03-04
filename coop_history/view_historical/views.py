a3d67458c293b4f5cb6d2bec8ba9fd14a77a8a1f
ShaneXie <shane369@gmail.com>
2015-03-04 06:24:59 -0600

    1. add title to each table 2. pull more datatable 3. add loading spinner

--- coop_history/view_historical/views.py
+++ coop_history/view_historical/views.py
@@ -13,4 +13,14 @@ def loadAjaxData(request, query):
     if query == 'getStudent':
         students = dataAccess.getStudentDT()
         return render_to_response('studentDT.html', {'students': students}, context_instance=RequestContext(request))
-    return HttpResponse(query)
+    elif query == 'getCompany':
+        companies = dataAccess.getCompanyDT()
+        return render_to_response('companyDT.html', {'companies': companies}, context_instance=RequestContext(request))
+    elif query == 'getContact':
+        contacts = dataAccess.getContactDT()
+        return render_to_response('contactDT.html', {'contacts': contacts}, context_instance=RequestContext(request))
+    elif query == 'getPlacement':
+        placements = dataAccess.getPlacementDT()
+        return render_to_response('placementDT.html', {'placements': placements}, context_instance=RequestContext(request))
+    else:
+        return HttpResponse("invalid ajax request")
